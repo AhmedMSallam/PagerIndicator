353905d7a9a5c8d116f5ae16cd879562348e88a5
Ahmed Sallam <theshadow499@gmail.com>
2022-01-14 16:54:53 +0200

    [add] initial project files

---
+++ app/src/main/java/com/ahmedsallam/pagerindicator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ahmedsallam.pagerindicator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
